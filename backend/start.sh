b00742c667c79871ede0339b1e9b02f2807e415f
Cristian Martin <cmf@lcc.uma.es>
2020-04-24 13:19:41 +0200

    Fixed bugs: not found tables in backend and folders of models didn't exist

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py makemigrations --noinput
-python manage.py migrate
+python manage.py migrate --run-syncdb
 gunicorn autoweb.wsgi:application --bind 0.0.0.0:8000
