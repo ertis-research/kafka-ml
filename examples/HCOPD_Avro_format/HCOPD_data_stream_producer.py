3394270fb5db0e7abbea6f98eecb314c94f17d78
Cristian Martin <cmf@lcc.uma.es>
2020-05-20 09:07:46 +0200

    Updated HCOPD example

--- examples/HCOPD_Avro_format/HCOPD_data_stream_producer.py
+++ examples/HCOPD_Avro_format/HCOPD_data_stream_producer.py
@@ -24,9 +24,9 @@ features = pd.DataFrame(preprocessing.scale(copd_data[copd_data_columns[copd_dat
 diagnosis = copd_data['Diagnosis']
 """Diagnosis column"""
 
-hcopd = AvroSink(boostrap_servers='127.0.0.1:9094', topic='hcopd', deployment_id=68, 
+hcopd = AvroSink(boostrap_servers='127.0.0.1:9094', topic='hcopd', deployment_id=1, 
         data_scheme_filename='data_scheme.avsc', label_scheme_filename='label_scheme.avsc',
-        description='Mnist dataset', validation_rate=0.33)
+        description='COPD dataset', validation_rate=0.1)
 
 for i in range (0, copd_data.shape[0]):
     data  = {"gender": features[0][i], "age": features[1][i], "smoking": features[2][i]}
