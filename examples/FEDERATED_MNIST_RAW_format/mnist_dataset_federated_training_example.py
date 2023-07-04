512e7e75732708d6aa7c4da264da9ee8f9a46eb7
Antonio Jesús <antoniochavesgarcia@uma.es>
2023-07-04 10:53:17 +0200

    feat (examples): New example for Federated Training use case.

---
+++ examples/FEDERATED_MNIST_RAW_format/mnist_dataset_federated_training_example.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append(sys.path[0] + "/../..") 
+"""To allow importing datasources"""
+
+from datasources.federated_raw_sink import FederatedRawSink
+from datasources.raw_sink import RawSink
+import tensorflow as tf
+import logging
+import json
+
+logging.basicConfig(level=logging.INFO)
+
+with open('examples/MNIST_RAW_format/mnist_sample_input_format.json') as json_file:
+    data_res = json.load(json_file)
+
+data_res = {}
+
+mnist = FederatedRawSink(boostrap_servers='192.168.43.7:32001', topic='mnist_fed', deployment_id=1, description='Mnist dataset', 
+                        dataset_restrictions=json.dumps(data_res), validation_rate=0.1, test_rate=0, control_topic='FEDERATED_DATA_CONTROL_TOPIC')
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+for (x, y) in zip(x_test, y_test):
+    mnist.send(data=x, label=y)
+
+mnist.close()
