975ac292e1210ff98759a9a6cc17cfce2c66fb8d
alexcarnero97uma <alexcarnero97@uma.es>
2024-03-01 11:29:21 +0100

    Resolved problem with metrics for distributed models

--- model_training/tensorflow/mainTraining.py
+++ model_training/tensorflow/mainTraining.py
@@ -286,6 +286,11 @@ class MainTraining(object):
 
         metrics = self.metrics.replace(' ', '')
         metrics = metrics.split(',')
+        for m in metrics:
+            if m == 'tf.keras.metrics.Precision()':
+                m = tf.keras.metrics.Precision()
+            elif m == 'tf.keras.metrics.Recall()':
+                m = tf.keras.metrics.Recall()
         """Formats metrics"""
 
         outputs = []
